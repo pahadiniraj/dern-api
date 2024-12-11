c47f8b6d292472fd8ece87d5daeb9de31d5fd26e
pahadiniraj <sharma12345niraj@gmail.com>
2024-12-11 09:32:43 +0545

    chnages in index.js app

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ dotenv.config();
 
 const app = express();
 
-// Configure CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // Allow your frontend's URL
-    credentials: true, // Allow cookies and Authorization headers
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
   })
 );
 
-// Handle preflight requests
 app.options("*", cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieparser());
@@ -34,5 +32,11 @@ app.use("/storage", express.static("storage"));
 
 LoadRoutes(app);
 
-// Export the app for Vercel
-module.exports = app;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+} else {
+  module.exports = app;
+}
